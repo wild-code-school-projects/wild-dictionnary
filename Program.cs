123ecd1dd116d6a93f0fc203d2ee36afdfaf850a
SuperCoolNinja <sbpro570@gmail.com>
2024-04-22 17:05:13 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, string>();
+            words.Add("a", "foo");
+            words.Add("b", "bar");
+            words.Add("c", "baz");
+
+            Console.WriteLine("Before : ");
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"\t{word}");
+            }
+
+            words.Remove("c");
+
+            Console.WriteLine();
+
+            Console.WriteLine("After: ");
+
+            foreach (var word in words)
+                Console.WriteLine($"\t{word}");
+        }
+    }
+}
